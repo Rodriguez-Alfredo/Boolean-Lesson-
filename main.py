9a1ff8183df7ef192ebc544bc8481cfaa856c512
AlfredoRodriguez <126662592+Rodriguez-Alfredo@users.noreply.github.com>
2023-03-10 22:49:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+#RELATIONAL AND BOOLEAN OPERATORS
+
+greater_than = 7 > 5
+less_than = 5 < 7
+freater_than_equal_to = 7 >= 7
+less_than_equal_to = 7 <= 7
+
+test_and = (7 > 5) and (5 < 7)  #Both statments must bee true
+
+test_and2 = (7 > 5) and (5 > 7)  #one statment is false, this line is False
+
+test_or = (7 > 5) or (
+  5 < 7)  #only needs one statment to be true to make it completly true
+
+test_or2 = (7 > 5) or (5 > 7)  #both have to be flase for it to be False
+
+test_not = not True  #makes this false
+test_not = not False  #makes true
+
+#Thank You TCM Security
